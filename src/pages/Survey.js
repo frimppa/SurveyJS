f6b28c851031128c8fce5ec08eae91596f27d203
Frimodig, Aleksi <aleksi.frimodig2@seamk.fi>
2021-10-22 14:32:28 +0300

    intial commit

---
+++ src/pages/Survey.js
@@ -0,0 +1,14 @@
+import React from "react";
+import react from React;
+
+class Survey extends React.Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Survey />
+)
