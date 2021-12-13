224820c41ca7a114941de15a47b4f98f89ede187
frimppa <aleksi.frimodig@gmail.com>
2021-12-13 17:28:20 +0200

    Added functionality to Rating-component

--- src/common/components/MaterialUI/Rating.js
+++ src/common/components/MaterialUI/Rating.js
@@ -10,10 +10,20 @@ import Paper from '@mui/material/Paper';
 import MuiGrid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import TestSurvey from "../TestSurvey.json";
+import Button from '@mui/material/Button';
+import { SettingsInputAntennaTwoTone } from "@mui/icons-material";
 //import JsonParser from "../Helpers/JsonParser";
 
 export default function BasicRating() {
-  const [value, setValue] = React.useState(2);
+  const [value, setValue] = React.useState({
+    "group1":{
+      "arvostelu0": 4
+    }
+  });
+  const [count, setCount] = React.useState(0);
+  const [completed, setCompleted] = React.useState(false);
+
+
   const style = {
     width: "100%",
     maxWidth: "100%",
@@ -27,22 +37,30 @@ export default function BasicRating() {
     },
   }));
 
-  const GetData = () => {
+  const handleClick = () => {
+    console.log("öajdöajsd")
+  }
 
+  const GetData = () => {
     return(
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+      {completed ? <h1>Kiitos vastaamisesta!</h1> :
       <div style={{ marginLeft: "10%" }}>
         <h1>{TestSurvey.title}</h1>
         <h3>{TestSurvey.description}</h3>
       <div
         style={{ display: "flex", justifyContent: "center", marginTop: "55px" }}
       >
-        {TestSurvey.pages.map((item, index) => (
-          <div>
+        {TestSurvey.pages.map((item, pageIndex) => (
+          
+          <div style={{display: "flex", flexDirection: "column"}}>
+            {pageIndex === count ?
+            <div>
             {item.groups.map((groupItem, index) => (
-              <Paper style={{padding: "5%"}}>
+              <Paper style={{padding: "5%", marginTop: "5%"}}>
                   <h2>{groupItem.title}</h2>
                   <List sx={style} component="nav" aria-label="mailbox folders">
-                    {groupItem.questions.map((questionItem, index) => (
+                    {groupItem.questions.map((questionItem, questionIndex) => (
                       <div>
                         <ListItem
                         button
@@ -62,10 +80,23 @@ export default function BasicRating() {
                             >
                               <h4 style={{width:"20%"}}>{questionItem.minRateDescription}</h4>
                               <Rating
-                                name="simple-controlled"
-                                value={value}
+                                name={`arvostelu${questionIndex}`}
+                                //value={typeof(value[groupItem.groupID][`arvostelu${questionIndex}`]) !== undefined ? value[groupItem.groupID][`arvostelu${questionIndex}`] : 0 }
+                                value={(groupItem.groupID in value) ? value[groupItem.groupID][`arvostelu${questionIndex}`] : 0 }
+                                //alue={value[groupItem.groupID].ok !== undefined ? value[groupItem.groupID] : 0 }
                                 onChange={(event, newValue) => {
-                                  setValue(newValue);
+                                  //console.log("TÄSSÄ", typeof(value[groupItem.groupID].hasOwnProperty([`arvostelu${questionIndex}`])))
+                                  groupItem.groupID in value ? console.log("löytyy") : console.log("ei löydy")
+                                  console.log(value.group1.arvostelu0)
+                                  let groupData = {}
+                                  if(value[groupItem.groupID]){
+                                    groupData = value[groupItem.groupID]
+                                  }
+                                                                      
+                                  groupData[`arvostelu${questionIndex}`] = newValue;
+                                  console.log("groupData", groupData)
+                                  setValue(prevState => ({ ...prevState, [groupItem.groupID] : groupData}))
+                                  //setValue(prevState =>({ ...prevState, [value[groupItem.groupID][`arvostelu${questionIndex}`]]: newValue}));
                                 }}
                                 size="extra-large"
                               />
@@ -80,15 +111,25 @@ export default function BasicRating() {
                   </List>
               </Paper>
             ))}
+             {TestSurvey.pages.length > 1 ?
+            <div style={{display: "flex", justifyContent: count > 0 ? "space-between" : "flex-end", marginBottom: "5%"}}>
+              {count > 0 ?
+            <Button variant="contained" style={{marginTop: "5%"}} onClick={() => setCount(count - 1)}>Edellinen</Button> : null}
+            {count+1 !== TestSurvey.pages.length ?
+            <Button variant="contained" style={{marginTop: "5%"}} onClick={() => setCount(count + 1)}>Seuraava</Button>  : null}   
+            {count+1 === TestSurvey.pages.length ?
+            <Button variant="contained" style={{marginTop: "5%"}} onClick={() => setCount(setCompleted(true))}>Valmis</Button> : null}      
+            </div> : null}
+            </div>
+          : null}
           </div>
         ))}
-
       </div>
+      </div>
+  }
       </div>
     )
-
   }
-
 
   return (
     <div>
