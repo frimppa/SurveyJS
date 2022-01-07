6bb28fa2bb32c45f3efe1b8629b601898964db7f
frimppa <aleksi.frimodig@gmail.com>
2022-01-07 15:59:25 +0200

    changed json parsing for reading EntreIntentio Backend form

--- src/common/components/MaterialUI/Rating.js
+++ src/common/components/MaterialUI/Rating.js
@@ -6,25 +6,51 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 import MuiGrid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
-import TestSurvey from "../TestSurvey.json";
-import Button from '@mui/material/Button';
-import Circle from '@mui/icons-material/Circle';
-import CircleOutlined from '@mui/icons-material/CircleOutlined';
+//import TestSurvey from "../TestSurvey.json";
+//import TestSurvey from "../BackendMock.json";
+import Button from "@mui/material/Button";
+import Circle from "@mui/icons-material/Circle";
+import CircleOutlined from "@mui/icons-material/CircleOutlined";
 import { SettingsInputAntennaTwoTone } from "@mui/icons-material";
+import axios from "axios";
 //import JsonParser from "../Helpers/JsonParser";
 
 export default function BasicRating() {
   const [value, setValue] = React.useState({
-    "group1":{
-      "arvostelu0": 4
-    }
+    group1: {
+      arvostelu0: 4,
+    },
   });
   const [count, setCount] = React.useState(0);
   const [completed, setCompleted] = React.useState(false);
+  const [TestSurvey, setTestSurvey] = React.useState({ data: {} });
 
+  const getDataFromBackend = () => {
+    console.log("PAINETTIIN");
+    axios.get("http://localhost:8080/get/test/").then((res) => {
+      console.log(res);
+      console.log(res.data);
+    });
+  };
+
+React.useEffect(() => {
+    console.log("Mounted....")
+    const fetchData = async () => {
+      const res = await fetch(
+        'http://localhost:8080/get/test/',
+      );
+      const json = await res.json();
+      /*console.log("objekti: ", json.data.pages)
+      Object.values(json.data.pages).map(item => {
+        console.log("ITEM: ", item)
+      })*/
+      setTestSurvey(json);
+    };
+    fetchData();
+  }, [setTestSurvey]);
 
   const style = {
     width: "100%",
@@ -40,7 +66,7 @@ export default function BasicRating() {
   }));
 
   const StyledRating = styled(Rating)({
-   /* '& .MuiRating-iconFilled': {
+    /* '& .MuiRating-iconFilled': {
       color: '#201751',
     },
     '& .MuiRating-iconHover': {
@@ -49,104 +75,201 @@ export default function BasicRating() {
   });
 
   const handleClick = () => {
-    console.log("öajdöajsd")
-  }
+    console.log("öajdöajsd");
+  };
 
   const GetData = () => {
-    return(
-      <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
-      {completed ? <h1>Kiitos vastaamisesta!</h1> :
-      <div style={{ marginLeft: "10%" }}>
-        <h1>{TestSurvey.title}</h1>
-        <h3>{TestSurvey.description}</h3>
+    return (
       <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "55px" }}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        {TestSurvey.pages.map((item, pageIndex) => (
-          
-          <div style={{display: "flex", flexDirection: "column"}}>
-            {pageIndex === count ?
-            <div>
-            {item.groups.map((groupItem, index) => (
-              <Paper style={{padding: "5%", marginTop: "5%"}}>
-                  <h2>{groupItem.title}</h2>
-                  <List sx={style} component="nav" aria-label="mailbox folders">
-                    {groupItem.questions.map((questionItem, questionIndex) => (
-                      <div>
-                        <ListItem
-                        button
-                        style={{ display: "flex", justifyContent: "space-between" }}
-                      >
-                        <Grid container style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                          <Grid item xs>
-                            <Typography component="legend">
-                              {questionItem.title}
-                            </Typography>
-                          </Grid>
-                          <Divider orientation="vertical" flexItem>
-                          </Divider>
-                          <Grid item xs>
-                            <div
-                              style={{ display: "flex", justifyContent: "space-evenly", alignItems: "center" }}
-                            >
-                              <h4 style={{width:"20%"}}>{questionItem.minRateDescription}</h4>
-                              <StyledRating
-                                icon={<Circle fontSize="inherit" />}
-                                emptyIcon={<CircleOutlined fontSize="inherit" />}
-                                name={`arvostelu${questionIndex}`}
-                                //value={typeof(value[groupItem.groupID][`arvostelu${questionIndex}`]) !== undefined ? value[groupItem.groupID][`arvostelu${questionIndex}`] : 0 }
-                                value={(groupItem.groupID in value) ? value[groupItem.groupID][`arvostelu${questionIndex}`] : 0 }
-                                //alue={value[groupItem.groupID].ok !== undefined ? value[groupItem.groupID] : 0 }
-                                onChange={(event, newValue) => {
-                                  //console.log("TÄSSÄ", typeof(value[groupItem.groupID].hasOwnProperty([`arvostelu${questionIndex}`])))
-                                  groupItem.groupID in value ? console.log("löytyy") : console.log("ei löydy")
-                                  console.log(value.group1.arvostelu0)
-                                  let groupData = {}
-                                  if(value[groupItem.groupID]){
-                                    groupData = value[groupItem.groupID]
-                                  }
-                                                                      
-                                  groupData[`arvostelu${questionIndex}`] = newValue;
-                                  console.log("groupData", groupData)
-                                  setValue(prevState => ({ ...prevState, [groupItem.groupID] : groupData}))
-                                  //setValue(prevState =>({ ...prevState, [value[groupItem.groupID][`arvostelu${questionIndex}`]]: newValue}));
+        {completed ? (
+          <h1>Kiitos vastaamisesta!</h1>
+        ) : (
+          <div style={{ marginLeft: "10%", width:"100vh" }}>
+            <h1>{TestSurvey.survey_title}</h1>
+            <h3>{TestSurvey.form_title}</h3>
+            <div
+              style={{
+                //display: "flex",
+                justifyContent: "center",
+                marginTop: "55px",
+              }}
+            >
+              {
+                console.log("sen pituus on: ", Object.values(TestSurvey.data.pages).length)
+              }
+              
+
+              {Object.values(TestSurvey.data.pages).length > 1 ? (
+                Object.values(TestSurvey.data.pages).map((item, pageIndex) => (
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  {pageIndex === count ? (
+                    <div>
+                      {Object.values(item.groups).map((groupItem, index) => (
+                        <Paper style={{ padding: "5%", marginTop: "5%" }}>
+                          <h2>Group Title</h2>
+                          <List
+                            sx={style}
+                            component="nav"
+                            aria-label="mailbox folders"
+                          >
+                            <div>
+                              {Object.values(groupItem).map((groupItem2)=>
+                              <ListItem
+                                button
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "space-between",
                                 }}
-                                size="extra-large"
-                              />
-                              <h4 style={{width:"20%"}}>{questionItem.maxRateDescription}</h4>
+                              >
+                                <Grid
+                                  container
+                                  style={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  <Grid item xs>
+                                    <Typography component="legend">
+                                      {groupItem2.title}
+                                    </Typography>
+                                  </Grid>
+                                  <Divider
+                                    orientation="vertical"
+                                    flexItem
+                                  ></Divider>
+                                  <Grid item xs>
+                                    <div
+                                      style={{
+                                        display: "flex",
+                                        justifyContent: "space-evenly",
+                                        alignItems: "center",
+                                      }}
+                                    >
+                                      <h4 style={{ width: "20%" }}>
+                                        {groupItem2.scale_min_title}
+                                      </h4>
+                                      <StyledRating
+                                        icon={<Circle fontSize="inherit" />}
+                                        emptyIcon={
+                                          <CircleOutlined fontSize="inherit" />
+                                        }
+                                        name={`arvostelu${groupItem2.question}`}
+                                        //value={typeof(value[groupItem.groupID][`arvostelu${questionIndex}`]) !== undefined ? value[groupItem.groupID][`arvostelu${questionIndex}`] : 0 }
+                                        value={
+                                          groupItem2.question in value
+                                            ? value[groupItem2.question][
+                                                `arvostelu${groupItem2.question}`
+                                              ]
+                                            : 0
+                                        }
+                                        //alue={value[groupItem.groupID].ok !== undefined ? value[groupItem.groupID] : 0 }
+                                        onChange={(event, newValue) => {
+                                          //console.log("TÄSSÄ", typeof(value[groupItem.groupID].hasOwnProperty([`arvostelu${questionIndex}`])))
+                                          groupItem2.question in value
+                                            ? console.log("löytyy")
+                                            : console.log("ei löydy");
+                                          //console.log(value.group1.arvostelu0)
+                                          let groupData = {};
+                                          if (value[groupItem2.question]) {
+                                            groupData =
+                                              value[groupItem2.question];
+                                          }
+
+                                          groupData[`arvostelu${groupItem2.question}`] =
+                                            newValue;
+                                          console.log("groupData", groupData);
+                                          setValue((prevState) => ({
+                                            ...prevState,
+                                            [groupItem2.question]: groupData,
+                                          }));
+                                          //setValue(prevState =>({ ...prevState, [value[groupItem.groupID][`arvostelu${questionIndex}`]]: newValue}));
+                                        }}
+                                        size="extra-large"
+                                      />
+                                      <h4 style={{ width: "20%" }}>
+                                        {groupItem2.scale_max_title}
+                                      </h4>
+                                    </div>
+                                  </Grid>
+                                </Grid>
+                              </ListItem>)}
+                              <Divider />
                             </div>
-                          </Grid>
-                        </Grid>
-                      </ListItem>
-                      <Divider/>
+                          </List>
+                        </Paper>
+                      ))}
+                      {Object.keys(TestSurvey.data.pages).length > 1 ? (
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent:
+                              count > 0 ? "space-between" : "flex-end",
+                            marginBottom: "5%",
+                          }}
+                        >
+                          {count > 0 ? (
+                            <Button
+                              variant="contained"
+                              style={{ marginTop: "5%" }}
+                              onClick={() => {
+                                window.scrollTo(0, 0);
+                                getDataFromBackend();
+                                setCount(count - 1);
+                              }}
+                            >
+                              Edellinen
+                            </Button>
+                          ) : null}
+                          {count + 1 !== Object.keys(TestSurvey.data.pages).length ? (
+                            <Button
+                              variant="contained"
+                              style={{ marginTop: "5%" }}
+                              onClick={() => {
+                                window.scrollTo(0, 0);
+                                getDataFromBackend();
+                                setCount(count + 1);
+                              }}
+                            >
+                              Seuraava
+                            </Button>
+                          ) : null}
+                          {count + 1 === Object.keys(TestSurvey.data.pages).length ? (
+                            <Button
+                              variant="contained"
+                              style={{ marginTop: "5%" }}
+                              onClick={() => {
+                                window.scrollTo(0, 0);
+
+                                setCount(setCompleted(true));
+                              }}
+                            >
+                              Valmis
+                            </Button>
+                          ) : null}
+                        </div>
+                      ) : null}
                     </div>
-                    ))}
-                  </List>
-              </Paper>
-            ))}
-             {TestSurvey.pages.length > 1 ?
-            <div style={{display: "flex", justifyContent: count > 0 ? "space-between" : "flex-end", marginBottom: "5%"}}>
-              {count > 0 ?
-            <Button variant="contained" style={{marginTop: "5%"}} onClick={() => setCount(count - 1)}>Edellinen</Button> : null}
-            {count+1 !== TestSurvey.pages.length ?
-            <Button variant="contained" style={{marginTop: "5%"}} onClick={() => setCount(count + 1)}>Seuraava</Button>  : null}   
-            {count+1 === TestSurvey.pages.length ?
-            <Button variant="contained" style={{marginTop: "5%"}} onClick={() => setCount(setCompleted(true))}>Valmis</Button> : null}      
-            </div> : null}
+                  ) : null}
+                </div>
+              ))) : null}
+              
             </div>
-          : null}
           </div>
-        ))}
+        )}
       </div>
-      </div>
-  }
-      </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
-      <GetData/>
+      <GetData />
     </div>
   );
 }
